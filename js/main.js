f7cddeae984b46734f4e0cde22b1c54f965972d8
Parves Hossain Rabby <56464115+Iamparves@users.noreply.github.com>
2020-01-29 15:04:37 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,52 +1,74 @@
 $(document).ready(function(){
-    var mixer = mixitup('.portfolio-content');
+  "use strict";
 
+  /***
+      Portfolio Filter
+  ***/
 
+  var mixer = mixitup('.portfolio-content');
 
-    $('.portfolio-full-img').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
-    });
-    
-    $('.team-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-          {
-            breakpoint: 1199,
-            settings: {
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 991,
-            settings: {
-              slidesToShow: 2
-            }
-          },
-          {
-            breakpoint: 575,
-            settings: {
-              slidesToShow: 1
-            }
+  /***
+      Portfolio Image Popup
+  ***/
+
+  $('.portfolio-full-img').magnificPopup({
+  type: 'image',
+  closeOnContentClick: true,
+  mainClass: 'mfp-img-mobile',
+  image: {
+    verticalFit: true
+  }
+  });
+  
+  /***
+      Team Slider
+  ***/
+
+  $('.team-slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3
           }
-        ]
-      });
-
-    $('.testimonial-slider').slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 4000
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
     });
 
+  /***
+      Testimonial Slider
+  ***/
 
+  $('.testimonial-slider').slick({
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 4000
+  });
 
+  /***
+      Counter
+  ***/
 
+  $(".counter").counterUp({
+    delay: 10,
+    time: 2500
+  })
+  
 });
